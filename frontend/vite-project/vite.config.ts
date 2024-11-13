b3072559642d4aed7664080cb315a1c794ef7a34
Mykhail <gorgefraggggg@gmail.com>
2024-11-14 00:29:40 +0300

    Fix bug in auth middleware

--- frontend/vite-project/vite.config.ts
+++ frontend/vite-project/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext', // Использование современных ES модуля
+    outDir: 'dist', // Папка для вывода файлов
+    assetsDir: 'assets',
+  },
   plugins: [react()],
 })
